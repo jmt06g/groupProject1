750a38ccbebc6e96bd42e31752f770f5137d4415
Janere Quijano <janerequijano@Janeres-MacBook-Air.local>
2018-07-02 18:28:11 -0400

    628

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-    $("#jobs").append('<li class="list-group-item">2job<a>Company Name</a> <p>Location</p></li>')
+    $("#jobs").append('<li class="list-group-item">2job<a>Company Name</a> <p>Location</p></li>');
+    showJob("example title","ex des","ex Cname", "ex apply")
 
 })
+function showJob(title, Des, Cname, apply)
+{$("#title").text(title);
+$("#Des").text(Des);
+$("#Cname").text(Cname);
+$("#apply").text(apply);
+}
