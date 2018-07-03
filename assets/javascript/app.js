ec3fba996b4b0b8a6ed42db447857fa2ce0642f2
Lucy <mayukuchler@gmail.com>
2018-07-02 21:22:01 -0400

    projectFullRemix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,37 @@
-$(document).ready(function(){
-    $("#jobs").append('<li class="list-group-item">2job<a>Company Name</a> <p>Location</p></li>');
-    showJob("example title","ex des","ex Cname", "ex apply")
+var jobs = [
+    "JavaScript",
+    "FrontEnd",
+    "BackEnd",
+  ];
+  var jobDescription = "";
 
-})
-function showJob(title, Des, Cname, apply)
-{$("#title").text(title);
-$("#Des").text(Des);
-$("#Cname").text(Cname);
-$("#apply").text(apply);
-}
+
+
+function createButton() {
+    $("#map").empty();
+    for (var i = 0; i < jobs.length; i++) {
+      var a = $("<button>");
+      a.attr("data-jobs", jobs[i]);
+      a.text(jobs[i]);
+      a.addClass("jobButton");
+      a.addClass("btn btn-outline-secondary");
+      $("#map").append(a);
+    }
+  }
+
+
+    $("#jobButton").on("click", function() {
+      var job = $(this).attr("data-jobs");
+      var queryURL =
+        "https://maps.googleapis.com/maps/api/js?key=" +
+        job +
+        "AIzaSyD3Zg5J4ZdXMk4EQR45MZNCy6SWjwZEVXc";
+
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        var results = response.data;
+      })
+    })
